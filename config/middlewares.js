6d49e739656e2c9700f9a9cbcad31aab0dab3b3e
nathzg <nathzg@gmail.com>
2023-02-18 09:24:30 -0500

    s3Bucket y lenguaje espanol configurado

--- config/middlewares.js
+++ config/middlewares.js
@@ -1,4 +1,32 @@
 module.exports = [
+  /* Beginning of snippet */
+  {
+    name: 'strapi::security',
+    config: {
+      contentSecurityPolicy: {
+        useDefaults: true,
+        directives: {
+          'connect-src': ["'self'", 'https:'],
+          'img-src': [
+            "'self'",
+            'data:',
+            'blob:',
+            'dl.airtable.com',
+            'strapi-storage-s3-bucket.s3-website.us-east-2.amazonaws.com',
+          ],
+          'media-src': [
+            "'self'",
+            'data:',
+            'blob:',
+            'dl.airtable.com',
+            'strapi-storage-s3-bucket.s3-website.us-east-2.amazonaws.com',
+          ],
+          upgradeInsecureRequests: null,
+        },
+      },
+    },
+  },
+  /* End of snippet */
   'strapi::errors',
   'strapi::security',
   'strapi::cors',
