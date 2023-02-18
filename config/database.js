b95a0809a9ca987508ed094ddeb6462061882d83
nathzg <nathzg@gmail.com>
2023-02-18 06:28:53 -0500

    postgres install
    Se agrega la base de datos postgres en aws

--- config/database.js
+++ config/database.js
@@ -1,11 +1,20 @@
 const path = require('path');
 
-module.exports = ({ env }) => ({
-  connection: {
-    client: 'sqlite',
-    connection: {
-      filename: path.join(__dirname, '..', env('DATABASE_FILENAME', '.tmp/data.db')),
-    },
-    useNullAsDefault: true,
-  },
-});
+    // strapi-api/config/database.js
+    module.exports = ({ env }) => ({
+      connection: {
+        client: 'postgres',
+        connection: {
+          host: env('DATABASE_HOST', 'pontegeekdb.cvkksi1ymser.us-east-2.rds.amazonaws.com'),
+          port: env.int('DATABASE_PORT', 5432),
+          database: env('DATABASE_NAME', 'postgres'),
+          user: env('DATABASE_USERNAME', 'pontedba'),
+          password: env('DATABASE_PASSWORD', 'ponteGeekDBA01.'),
+          schema: env('DATABASE_SCHEMA', 'public'), // Not required
+          ssl: {
+            rejectUnauthorized: env.bool('DATABASE_SSL_SELF', false),
+          },
+        },
+        debug: false,
+      },
+    });
