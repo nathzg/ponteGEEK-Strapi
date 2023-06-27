3042dd365f992ae0caa9d353a6242a1091963e23
nathzg <nathzg@gmail.com>
2023-06-27 12:04:28 -0500

    Upgrade 4.11.2

--- config/plugins.js
+++ config/plugins.js
@@ -1,7 +1,20 @@
     // ~/strapi-aws-s3/backend/config/plugins.js
     
     module.exports = ({ env }) => ({
-        upload: {
+      upload: {
+        provider: 'aws-s3',
+        providerOptions: {
+          s3Options: {
+            accessKeyId: env('AWS_ACCESS_KEY_ID'),
+            secretAccessKey: env('AWS_ACCESS_SECRET'),
+            region: env('AWS_REGION'),
+          },
+          params: {
+            Bucket: env('AWS_BUCKET'),
+          },
+        },
+      },
+        /*upload: {
           config: {
             provider: 'aws-s3',
             providerOptions: {
@@ -19,10 +32,11 @@
               delete: {},
             },
           },
-        },
+        },*/
         //ckeditor5: {
         //  enabled: true,
           //resolve: "./src/plugins/strapi-plugin-ckeditor"
         //},
       });
       
+
