456d3e97206db5f53eae219a5af6f9cafeb496c1
nathzg <nathzg@gmail.com>
2025-05-27 09:52:42 -0500

    Update database.js

--- config/database.js
+++ config/database.js
@@ -5,11 +5,11 @@ const path = require('path');
       connection: {
         client: 'postgres',
         connection: {
-          host: env('DATABASE_HOST', 'pontegeekdb.cvkksi1ymser.us-east-2.rds.amazonaws.com'),
+          host: env('DATABASE_HOST', 'pontegeek-db.cvkksi1ymser.us-east-2.rds.amazonaws.com'),
           port: env.int('DATABASE_PORT', 5432),
           database: env('DATABASE_NAME', 'postgres'),
           user: env('DATABASE_USERNAME', 'pontedba'),
-          password: env('DATABASE_PASSWORD', 'pytbyt-pirfe3-cajRyf'),
+          password: env('DATABASE_PASSWORD', 'ponteGeekDBA01.'),
           schema: env('DATABASE_SCHEMA', 'public'), // Not required
           ssl: {
             rejectUnauthorized: env.bool('DATABASE_SSL_SELF', false),
