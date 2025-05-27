d86bdb5506f6bda8019a3a87c32f6e29d5a0ee6c
nathzg <nathzg@gmail.com>
2025-05-27 09:14:53 -0500

    Update database.js

--- config/database.js
+++ config/database.js
@@ -9,7 +9,7 @@ const path = require('path');
           port: env.int('DATABASE_PORT', 5432),
           database: env('DATABASE_NAME', 'postgres'),
           user: env('DATABASE_USERNAME', 'pontedba'),
-          password: env('DATABASE_PASSWORD', 'ponteGeekDBA01.'),
+          password: env('DATABASE_PASSWORD', 'pytbyt-pirfe3-cajRyf'),
           schema: env('DATABASE_SCHEMA', 'public'), // Not required
           ssl: {
             rejectUnauthorized: env.bool('DATABASE_SSL_SELF', false),
